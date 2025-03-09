6d3f05b1002da51d9f63d608aa4b0f4dd9e5bc1a
Francisco Pedrosa <francsp1@hotmail.com>
2025-03-09 23:08:03 +0000

    changes to queue

--- inc/lib/queue_thread_safe.h
+++ inc/lib/queue_thread_safe.h
@@ -15,8 +15,9 @@ typedef struct queue_thread_safe {
 queue_thread_safe_t *queue_create_thread_safe();
 void queue_insert_thread_safe(void * element, queue_thread_safe_t *queue);
 void *queue_remove_thread_safe(queue_thread_safe_t *queue);
-void* queue_remove_thread_safe_with_condition(queue_thread_safe_t *queue, volatile sig_atomic_t *keep_running);
+void* queue_remove_thread_safe_with_condition(queue_thread_safe_t *queue, sig_atomic_t volatile *keep_running);
 void *queue_query_first_thread_safe(queue_thread_safe_t *queue);
+void _free_queue_and_destroy_elements(queue_thread_safe_t **queue);
 void _destroy_thread_safe(queue_thread_safe_t **queue);
 #define queue_destroy_thread_safe(queue) _destroy_thread_safe((queue_thread_safe_t **)(&(queue)))
 uint32_t queue_get_number_of_elements_thread_safe(queue_thread_safe_t *queue);
